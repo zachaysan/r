705cc7a0ca615071310909802550e1d10e5f3b6f
Zach Aysan <zachaysan@gmail.com>
2013-02-16 20:17:16 -0500

    Init project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// import SDL
+
+// import standard c++ stuff
+
+// import other classes
+
+// set constants
+
+// set globals
+
+// init
+
+// load files (maybe in linked files)
+
+// init main
+
+// clean up
